f3445fd54a8f9cddbd809dcc9aaa79978835455c
hslin990803 <hslin0803@gmail.com>
2025-09-04 18:49:13 +0900

    25.09.04 Add program file & 백준 2문제

---
+++ baekjoon/Project1/practice.cpp
@@ -0,0 +1,18 @@
+// 백준 #2557
+//#include <stdio.h>
+//int main() {
+//	printf("Hello World!");
+//
+//	return 0;
+//}
+
+// 백준 #1000
+//#include <stdio.h>
+//int main() {
+//	int a, b;
+//
+//	scanf("%d %d", &a, &b);
+//	printf("%d", a+b);
+//
+//	return 0;
+//}
