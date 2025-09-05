b5b1f2918cceb2315b7627151756483d592dac42
hslin990803 <hslin0803@gmail.com>
2025-09-05 20:44:16 +0900

    25.09.05 백준 1단계 완료

---
+++ baekjoon/Project1/level_1.cpp
@@ -0,0 +1,161 @@
+// 백준 #2557
+//#include <stdio.h>
+//int main() {
+//	printf("Hello World!");
+//
+//	return 0;
+//}
+
+// 백준 #1000
+//#include <stdio.h>
+//int main() {
+//	int a, b;
+//
+//	scanf("%d %d", &a, &b);
+//	printf("%d", a+b);
+//
+//	return 0;
+//}
+
+// 백준 #1001
+//#include <stdio.h>
+//int main() {
+//	int a, b;
+// 
+//	scanf("%d %d", &a, &b);
+//	printf("%d", a - b);
+//
+//	return 0;
+//}
+
+// 백준 #10998
+//#include <stdio.h>
+//int main() {
+//	int a, b;
+//
+//	scanf("%d %d", &a, &b);
+//	printf("%d", a * b);
+//
+//	return 0;
+//}
+
+// 백준 #1008
+//#include <stdio.h>
+//int main() {
+//	int  a, b;
+//
+//	scanf("%d %d", &a, &b);
+//	printf("%.9f", (double)a / b);
+//
+//	return 0;
+//}
+
+// 백준 #10869
+//#include <stdio.h>
+//int main() {
+//	int a, b;
+//
+//	scanf("%d %d", &a, &b);
+//	printf("%d\n", a + b);
+//	printf("%d\n", a - b);
+//	printf("%d\n", a * b);
+//	printf("%d\n", a / b);
+//	printf("%d\n", a % b);
+//
+//	return 0;
+//}
+
+// 백준 #10926
+//#include <stdio.h>
+//int main() {
+//	char str[50];
+//
+//	scanf("%50s", &str);
+//	printf("%s??!", str);
+//
+//	return 0;
+//}
+
+// 백준 #18108
+//#include <stdio.h>
+//int main() {
+//	int a;
+//
+//	scanf("%d", &a);
+//	printf("%d", a - 543);
+//
+//	return 0;
+//}
+
+// 백준 #10430
+//#include <stdio.h>
+//int main() {
+//	int a, b, c;
+//
+//	scanf("%d %d %d", &a, &b, &c);
+//	printf("%d\n", (a + b) % c);
+//	printf("%d\n", ((a % c) + (b % c)) % c);
+//	printf("%d\n", (a * b) % c);
+//	printf("%d\n", ((a % c) * (b % c)) % c);
+//
+//	return 0;
+//}
+
+// 백준 #2588
+//#include <stdio.h>
+//int main() {
+//	int a, b;
+//	int i, j, k;
+//	int temp;
+//
+//	scanf("%d", &a);
+//	scanf("%d", &b);
+//
+//	temp = b;
+//	i = temp / 100;
+//	temp -= i * 100;
+//	j = temp / 10;
+//	temp -= j * 10;
+//	k = temp;
+//
+//	printf("%d\n", a * k);
+//	printf("%d\n", a * j);
+//	printf("%d\n", a * i);
+//	printf("%d\n", a * b);
+//
+//	return 0;
+//}
+
+// 백준 #11382
+//#include <stdio.h>
+//int main() {
+//	long long a, b, c;
+//
+//	scanf("%lld %lld %lld", &a, &b, &c);
+//	printf("%lld", a + b + c);
+//
+//	return 0;
+//}
+
+// 백준 #10171
+//#include <stdio.h>
+//int main() {
+//    printf("\\    /\\\n");
+//    printf(" )  ( ')\n");
+//    printf("(  /  )\n");
+//    printf(" \\(__)|\n");
+//
+//	return 0;
+//}
+
+// 백준 #10172
+//#include <stdio.h>
+//int main() {
+//    printf("|\\_/|\n");
+//    printf("|q p|   /}\n");
+//    printf("( 0 )\"\"\"\\\n");
+//    printf("|\"^\"`    |\n");
+//    printf("||_/=\\\\__|\n");
+//
+//	return 0;
+//}
